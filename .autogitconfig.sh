aaa892ac1cb571c91748fb4a78ddd503d18b5403
dtork <dtorkelson@gmail.com>
2022-09-10 15:16:28 +0000

    add logic to show name has changed

--- .autogitconfig.sh
+++ .autogitconfig.sh
@@ -3,5 +3,14 @@
 # Supplemental git configurations 
 # (run this on setup of a new dotfile system)
 
-git config --global user.name "$USER@$HOSTNAME"
+previous_value="$(git config --get user.name)"
+new_value="$USER@$HOSTNAME"
+
+git config --global user.name "$new_value"
+
+if [ "${new_value}" == "${previous_value}" ]; then
+       exit 1
+else
+       exit 0
+fi
 
